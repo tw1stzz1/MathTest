f836fee102b4baaac73a76b60465e2da1a6a8f42
Tkaz <73423742+tw1stzz1@users.noreply.github.com>
2026-09-16 23:06:49 +0300

    added partial realization for MathTest class(also moved Task realization to .cpp)

--- Lib/Lib.h
+++ Lib/Lib.h
@@ -27,35 +27,97 @@ class MathTest {
 	int* _user_answers;
 	int _correct_answers_count;
 public:
-	MathTest(int questons_amount);
-	MathTest(int questons_amount, int min, int max);
-	MathTest(int questons_amount, int min, int max, int operation_num);
+	MathTest(int questons_count);
+	MathTest(int questons_count, int min, int max);
+	MathTest(int questons_count, int min, int max, int operation_num);
 
 	Task* tasks() const noexcept;
 	int questions_count();
 	int* get_user_answers() const noexcept;
 	int correct_answers_count();
 
-	void tasks(Task*);
-	void questions_count(int);
-	void user_answers(int*);
 	void correct_answers_count(int);
 
+	void create_tasks(int min = 0, int max = 0, char operation_num = '\0');
 	void run();
-	int get_questions_amount();
-	void create_tasks();
-	void check_user_answer();
+	void question_user();
+	void check_user_answer(int, int);
 	void show_stat();
 };
 
 int* MathTest::get_user_answers() const noexcept {
-	return  _user_answers;
+	return _user_answers;
 }
 
-MathTest::MathTest() {
-
+int MathTest::questions_count() {
+	return _questions_count;
 }
+
+MathTest::MathTest(int questons_count) {
+	_questions_count = questons_count;
+	create_tasks(); // this -> ????????????????
+	run();
+}
+MathTest::MathTest(int questons_count, int min, int max) {
+	_questions_count = questons_count;
+	create_tasks(min, max);
+	run();
+}
+MathTest::MathTest(int questons_count, int min, int max, int operation_num) {
+	_questions_count = questons_count;
+	create_tasks(min, max, operation_num);
+	run();
+}
+
 
 void MathTest::run() {
+	std::cout << "Now you will be subjected to test" << std::endl;
+	question_user();
 
+
+}
+
+void MathTest::question_user() {
+	for (int i = 0; i < _questions_count; i++) {
+		char oper;
+		int user_answer;
+		switch (_tasks[i].operation){
+		case Add:
+			oper = '+';
+			break;
+		case Subtract:
+			oper = '-';
+			break;
+		case Multiplication:
+			oper = '*';
+			break;
+		case Division:
+			oper = '/';
+			break;
+		}
+		std::cout << _tasks[i].num_1 << oper << _tasks[i].num_2 << '= ';
+		std::cin >> user_answer;
+		check_user_answer(user_answer, _tasks[i].answer);
+		std::cout << std::endl;
+		system("cls");
+	}
+}
+
+void MathTest::create_tasks(int min, int max, char operation_num) {
+	if(min == 0 && max == 0) {
+		for (int i = 0; i < _questions_count; i++) {
+			Task t;
+			_tasks[i] = t;
+		}
+	}
+	else {
+		for (int i = 0; i < _questions_count; i++) {
+			Task t(min, max, operation_num);
+			_tasks[i] = t;
+		}
+	}
+}
+
+void MathTest::check_user_answer(int user_answer, int answer) {
+	if 
 }
